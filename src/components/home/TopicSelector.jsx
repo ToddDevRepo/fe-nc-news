5fddba945f471df7d1a12afe628e47724e751a06
ToddDevRepo <todd@LAPTOP-AJE60IS6.localdomain>
2022-08-22 16:43:45 +0100

    Add topic selector box with default All Topics option

--- src/components/home/TopicSelector.jsx
+++ src/components/home/TopicSelector.jsx
@@ -5,7 +5,24 @@ const TopicSelector = () => {
   const [availableTopics, setAvailableTopics] = useState([
     DefaultTopics.TOPICS_ALL,
   ]);
-  return <section id="section__topic-selector">Selector</section>;
+  return (
+    <section id="section__topic-selector">
+      <select
+        onChange={(event) => {
+          //setSelectedTopic(event.target.value);
+        }}
+        name="topic-selector"
+      >
+        {availableTopics.map((topic) => {
+          return (
+            <option key={topic.slug} value={topic.slug}>
+              {topic.slug}
+            </option>
+          );
+        })}
+      </select>
+    </section>
+  );
 };
 
 export default TopicSelector;
