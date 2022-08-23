ec9f4b866e04ed6434269f0aa8f97a5cacbfb5b4
ToddDevRepo <todd@LAPTOP-AJE60IS6.localdomain>
2022-08-23 10:16:49 +0100

    Add global internal endpoints and use them to set up page page for home and article view

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/home/Home";
+import { InternalEndpoints } from "./Globals";
+import ArticleView from "./components/article/ArticleView";
 
 function App() {
   return (
@@ -9,7 +11,11 @@ function App() {
       <Header />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path={InternalEndpoints.HOME_END} element={<Home />} />
+          <Route
+            path={InternalEndpoints.ARTICLES_BY_ID_END}
+            element={<ArticleView />}
+          />
         </Routes>
       </main>
     </div>
