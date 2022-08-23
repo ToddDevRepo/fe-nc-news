730e851d63a96b2acf1a84657b4d1e74a1e3df43
ToddDevRepo <todd@LAPTOP-AJE60IS6.localdomain>
2022-08-23 15:07:31 +0100

    Fix - Add functionality to return home when header clicked

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+import { InternalEndpoints } from "../Globals";
+
 const Header = () => {
   return (
     <header className="app-header">
-      <h1>NC News App</h1>
+      <h1>
+        <Link to={InternalEndpoints.HOME_END}>NC News App</Link>
+      </h1>
       <h2>A place for all your NC news!</h2>
     </header>
   );
