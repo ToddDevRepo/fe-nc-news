f92ef85de880c263168298cffa82d6f5b2e17cae
ToddDevRepo <todd@LAPTOP-AJE60IS6.localdomain>
2022-08-23 10:38:56 +0100

    Add functionality so clicking article takes you to an empty page displaying article id

--- src/components/article/ArticleView.jsx
+++ src/components/article/ArticleView.jsx
@@ -1,3 +1,15 @@
-const ArticleView = () => {};
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+const ArticleView = () => {
+  const { article_id } = useParams();
+  useEffect(() => {}, []);
+
+  return (
+    <section className="section__article-view">
+      Article id: {article_id}
+    </section>
+  );
+};
 
 export default ArticleView;
--- src/components/home/ArticlesList.jsx
+++ src/components/home/ArticlesList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
-import { DefaultTopics } from "../../Globals";
+import { useNavigate } from "react-router-dom";
+import { DefaultTopics, InternalEndpoints } from "../../Globals";
 import {
   getAllArticles,
   getArticlesFilteredByTopic,
@@ -17,13 +18,20 @@ const ArticlesList = ({ selectedTopic }) => {
       setNewsArticles(articles);
     });
   }, [selectedTopic]);
+  const nav = useNavigate();
 
   return (
     <section id="section__articles-list">
       <ul className="list__undecorated">
         {newsArticles.map((article) => {
           return (
-            <li key={article.article_id} className="list__article-item">
+            <li
+              key={article.article_id}
+              className="list__article-item"
+              onClick={() => {
+                nav(`${InternalEndpoints.ARTICLES_END}/${article.article_id}`);
+              }}
+            >
               <ArticlesListItem article={article} />;
             </li>
           );
