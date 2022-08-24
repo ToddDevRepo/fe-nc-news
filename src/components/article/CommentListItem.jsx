fa5b5dbd03e577d574e334f116b816e98cdd9428
ToddDevRepo <todd@LAPTOP-AJE60IS6.localdomain>
2022-08-24 09:10:40 +0100

    Implement functionality for displaying all comment information.

--- src/components/article/CommentListItem.jsx
+++ src/components/article/CommentListItem.jsx
@@ -1,6 +1,16 @@
+import { timeStamp2Date } from "../../utils/time-utils";
+
 const CommentListItem = ({ comment }) => {
-  // comment_id, body, author, votes, created_at
-  return <h6>{comment.author}</h6>;
+  return (
+    <>
+      <h6>{comment.author}</h6>
+      <p>
+        <span>Created: {timeStamp2Date(comment.created_at)} </span>
+        <span>Votes: {comment.votes} </span>
+      </p>
+      <p>{comment.body}</p>;
+    </>
+  );
 };
 
 export default CommentListItem;
